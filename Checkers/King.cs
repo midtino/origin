fe413f5a6809ae709c19be62ce534e0656d020e4
midtino <jchiyoka@gmail.com>
2018-04-12 15:40:16 +0200

    Create class stubs

---
+++ Checkers/King.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checkers
+{
+    class King : IPiece
+    {
+        public Status Status => throw new NotImplementedException();
+
+        public Color Color => throw new NotImplementedException();
+
+        public int Position => throw new NotImplementedException();
+
+        public IEnumerable<int> CapturingMoves(IBoard board)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(int destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> NormalMoves(IBoard board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
